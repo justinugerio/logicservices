c28508c3ca4e5b3803bf5f651d5ae658e2cd2115
Justin Ugerio <justinugerio@users.noreply.github.com>
2015-07-02 11:25:33 -0700

    Major changes such as move HTML to Javascript and added Gantt engineer areas.
    Code more flexible.
    
    Signed-off-by: Justin Ugerio <justinugerio@users.noreply.github.com>

--- client/LogicServices.js
+++ client/LogicServices.js
@@ -2,27 +2,27 @@
 // main start function
 $(function() {
 
+    var engineers = ['Andy James', 'Harold Johnson', 'Lakiesha Hill'],
+        hours     = ['8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm'];
+
     // initialize Gantt table
-    LogicServices.initTable(LogicServices.Engineers, LogicServices.Hours);
+    LogicServices.initTable(engineers, hours);
 
     // initialize Gantt area
     LogicServices.initGanttArea();
 
     // add timeline to Gantt area
-    LogicServices.addTimeline('timeline-id', LogicServices.GanttAreaID);
-    $('.draggable-timeline').draggable({ axis: 'x', containment: '#' + LogicServices.GanttAreaID });
+    LogicServices.addTimeline();
 
 
     // create tasks
-    $('.draggable-task').draggable({ axis: 'x', containment: '#' + LogicServices.GanttAreaID })
+    $('.draggable-task').draggable({ axis: 'x', containment: '#gantt-area-id' })
       .resizable({ maxHeight: 30, minHeight: 30 });
 
 
 
-
-    /// testing code
+    /// testing code below here
     /////////////////////////////////////////////
-
 
 
 });
