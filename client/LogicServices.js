2456e41172244f683ab4426632abefa3c8a8087e
Justin Ugerio <justinugerio@users.noreply.github.com>
2015-07-01 17:44:13 -0700

    Moved elements into Javascript code.
    More flexibility.
    
    Signed-off-by: Justin Ugerio <justinugerio@users.noreply.github.com>

--- client/LogicServices.js
+++ client/LogicServices.js
@@ -1,22 +1,30 @@
 
-
 // main start function
 $(function() {
 
     // initialize Gantt table
     LogicServices.initTable(LogicServices.Engineers, LogicServices.Hours);
 
+    // initialize Gantt area
+    LogicServices.initGanttArea();
+
+    // add timeline to Gantt area
+    LogicServices.addTimeline('timeline-id', LogicServices.GanttAreaID);
+
 
     // create tasks
-    $('.draggable-task').draggable({ axis: 'x', containment: '#gantt-table-container-id' })
+    $('.draggable-task').draggable({ axis: 'x', containment: '#' + LogicServices.GanttAreaID })
       .resizable({ maxHeight: 30, minHeight: 30 });
-    $('.draggable-timeline').draggable({ axis: 'x', containment: '#gantt-table-container-id' });
+    $('.draggable-timeline').draggable({ axis: 'x', containment: '#' + LogicServices.GanttAreaID });
 
 
 
-    /// testing scroll values
+    /// testing
     var xvar = $('#gantt-table-container-id');
     xvar = xvar.get(0);
     console.log('Yoo hoo!' + ' ' + xvar.scrollLeft + ' ' + xvar.scrollTop + ' ' + xvar.scrollWidth + ' ' + xvar.scrollHeight);
+
+    var yvar = LogicServices.getGanttOffset();
+    console.log('Number 2: ' + yvar.x + ' ' + yvar.y);
 
 });
--- client/LogicServicesLib.js
+++ client/LogicServicesLib.js
@@ -8,16 +8,24 @@ LogicServices = (function () {
 
     // variable declarations
     var Engineers = ['Andy James', 'Harold Johnson', 'Lakesha Hill'],
-          Hours = ['8am', '9am', '10am', '11am', '12am', '1pm','2pm','3pm','4pm', '5pm'];
+          Hours = ['8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm'],
+          GanttAreaBuffer = { leftBuffer: 3, topBuffer: 3, widthBuffer: 3, heightBuffer: 2},
+          TableOffsetElementID = 'table-offset-element-id', // id's needed to retrieve element to calculate offset;
+          ColumnOffsetElementID = 'column-offset-element-id',
+          GanttAreaID = 'gantt-area-id';
 
     // function declarations
-    var initTable;
+    var initTable, initGanttArea, getGanttOffset, getColumnOffset, getNumberEngineers, getNumberHours, addTimeline;
 
 
     // functions
     /////////////////////////////////////////////
 
+    // initialize table based on engineers and hours provided
     initTable = function (engineers, hours) {
+
+        Engineers = engineers;
+        Hours = hours;
 
         var table = $('#gantt-table-id');
         var tHeader, tBody, tRow, tHead, tData;
@@ -30,6 +38,7 @@ LogicServices = (function () {
         tRow = $('<tr>');
         tHead = $('<th>').text('Engineer');    // initial 'Engineer' heading
         tHead.addClass('col-lg-2');
+        tHead.attr('id', TableOffsetElementID);     // set this ID so that we can later retrieve it for configuring the Gantt area
         tRow.append(tHead);
 
         // for each hour, create a heading for the table
@@ -37,6 +46,10 @@ LogicServices = (function () {
             tHead = $('<th>').text(Hours[i]);
             tHead.addClass('col-lg-1');
             tRow.append(tHead);
+
+            if (i == 0) {
+                tHead.attr('id', ColumnOffsetElementID); // set this ID so that we can later retrieve it for Gantt area
+            }
         }
 
         tHeader = $('<thead>').append(tRow);    // create table header
@@ -62,16 +75,123 @@ LogicServices = (function () {
 
     };
 
+    // creates a div element on the Gantt table for drag & drop area to work with
+    initGanttArea = function () {
+
+        var ganttContainer, ganttOffset, ganttAreaDiv, ganttOffsetX, ganttOffsetY, columnOffset,
+            numEngineers, numHours, leftBuffer, topBuffer, widthBuffer, heightBuffer;
+
+        ganttContainer = $('#gantt-table-container-id');
+
+        if (!ganttContainer) {
+            return;
+        }
+
+        ganttOffset = getGanttOffset();
+        ganttOffsetX = ganttOffset.x;
+        ganttOffsetY = ganttOffset.y;
+
+        columnOffset = getColumnOffset();
+
+        numEngineers = getNumberEngineers();
+        numHours = getNumberHours();
+
+        ganttAreaDiv = $('<div>').addClass('gantt-area');
+        ganttAreaDiv.attr('id', GanttAreaID);
+
+        ganttAreaDiv.css({
+            'left': (ganttOffsetX + GanttAreaBuffer.leftBuffer) + 'px',
+            'top': (ganttOffsetY + GanttAreaBuffer.topBuffer) + 'px',
+            'width': ((columnOffset * numHours) + GanttAreaBuffer.widthBuffer) + 'px',
+            'height': ((ganttOffsetY * numEngineers) + GanttAreaBuffer.heightBuffer) + 'px'
+        });
+
+        ganttContainer.append(ganttAreaDiv);
+    };
+
+    // returns the x and y offset for the Gantt area
+    getGanttOffset = function () {
+
+        var returnVal = { x: 0, y: 0 };
+
+        var element = $('#' + TableOffsetElementID);
+
+        if (!element) {
+            return returnVal;
+        }
+
+        element = element.get(0);
+        returnVal.x = element.scrollWidth;
+        returnVal.y = element.scrollHeight;
+
+        return returnVal;
+    };
+
+    // returns the width offset for a column of the Hours
+    getColumnOffset = function () {
+
+        var element = $('#' + ColumnOffsetElementID);
+
+        if (!element) {
+            return 0;
+        }
+
+        element = element.get(0);
+        return element.scrollWidth;
+
+    };
+
+    // returns the number of Engineers
+    getNumberEngineers = function () {
+        return Engineers.length;
+    };
+
+    // returns the number of Hours
+    getNumberHours = function () {
+        return Hours.length;
+    };
+
+    // add a Timeline to the specified container
+    addTimeline = function (id, container) {
+
+        var container = $('#' + GanttAreaID);
+        var timeline, numEngineers, ganttOffset, heightBuffer, leftBuffer;
+
+        if (!container) {
+            return;
+        }
+
+        timeline = $('<div>').attr('id', id).addClass('timeline ui-widget-content draggable-timeline');
+
+        numEngineers = getNumberEngineers();
+        ganttOffset = getGanttOffset();
+        heightBuffer = 3;
+        leftBuffer = -1;
+        timeline.css({
+            'left': leftBuffer,
+            'height': ((ganttOffset.y * numEngineers) + heightBuffer) + 'px'
+        });
+
+        container.append(timeline);
+
+    };
+
 
     // return object
     /////////////////////////////////////////////
     return {
 
         Engineers: Engineers,
-
         Hours: Hours,
+        GanttAreaID: GanttAreaID,
 
-        initTable: initTable
+        initTable: initTable,
+        initGanttArea: initGanttArea,
+        addTimeline: addTimeline,
+
+        getGanttOffset: getGanttOffset,
+        getNumberEngineers: getNumberEngineers,
+        getNumberHours: getNumberHours
 
     };
 
