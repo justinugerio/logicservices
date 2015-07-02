2bd8804eea622d364b04349fd62ed61ef321319e
Justin Ugerio <justinugerio@users.noreply.github.com>
2015-07-01 22:59:08 -0700

    Signed-off-by: Justin Ugerio <justinugerio@users.noreply.github.com>

--- client/LogicServices.js
+++ client/LogicServices.js
@@ -10,21 +10,19 @@ $(function() {
 
     // add timeline to Gantt area
     LogicServices.addTimeline('timeline-id', LogicServices.GanttAreaID);
+    $('.draggable-timeline').draggable({ axis: 'x', containment: '#' + LogicServices.GanttAreaID });
 
 
     // create tasks
     $('.draggable-task').draggable({ axis: 'x', containment: '#' + LogicServices.GanttAreaID })
       .resizable({ maxHeight: 30, minHeight: 30 });
-    $('.draggable-timeline').draggable({ axis: 'x', containment: '#' + LogicServices.GanttAreaID });
 
 
 
-    /// testing
-    var xvar = $('#gantt-table-container-id');
-    xvar = xvar.get(0);
-    console.log('Yoo hoo!' + ' ' + xvar.scrollLeft + ' ' + xvar.scrollTop + ' ' + xvar.scrollWidth + ' ' + xvar.scrollHeight);
 
-    var yvar = LogicServices.getGanttOffset();
-    console.log('Number 2: ' + yvar.x + ' ' + yvar.y);
+    /// testing code
+    /////////////////////////////////////////////
+
+
 
 });
