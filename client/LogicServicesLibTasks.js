939351bdb97a5552a0fe22cf24bb797e914226c0
Justin Ugerio <justinugerio@users.noreply.github.com>
2015-07-01 21:49:02 -0700

    Moved elements into Javascript code.
    More flexibility.
    
    Signed-off-by: Justin Ugerio <justinugerio@users.noreply.github.com>

---
+++ client/LogicServicesLibTasks.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Justin.Ugerio on 7/1/2015.
+ */
+
+// LogicServices global namespace var
+// Self-calling function
+LogicServices.Task = (function () {
+
+    // variable declarations
+    var addTask;
+
+
+    // functions
+    /////////////////////////////////////////////
+    addTask = function (name, containerID) {
+
+        var container = $('#' + containerID);
+
+    };
+
+
+
+    // return object
+    /////////////////////////////////////////////
+    return {
+
+        addTask: addTask
+
+    };
+
+}) ();
